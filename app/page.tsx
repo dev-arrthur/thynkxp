2838fe3904bcf74cc128cff1dc32306a8e0eddce
devarrthur <dev.arrthur@gmail.com>
2026-08-31 14:04:33 -0300

    fix: ativar marquees contínuos na home

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import HomeExperience from '../components/HomeExperience';
 import AnalyticsTracker from '../components/AnalyticsTracker';
+import ContinuousMarquees from '../components/ContinuousMarquees';
 
 export default function Home() {
   return (
     <>
       <HomeExperience />
+      <ContinuousMarquees />
       <AnalyticsTracker />
     </>
   );
