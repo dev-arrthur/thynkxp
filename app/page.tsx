2fb0b8160165b146f0e8f13269d4e87aa5e9e0ef
devarrthur <dev.arrthur@gmail.com>
2026-09-01 11:30:35 -0300

    feat: apply live navbar sync

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import HomeExperience from '../components/HomeExperience';
 import AnalyticsTracker from '../components/AnalyticsTracker';
 import ContinuousMarquees from '../components/ContinuousMarquees';
 import HomeChromeEnhancer from '../components/HomeChromeEnhancer';
+import NavbarLiveSync from '../components/NavbarLiveSync';
 
 export default function Home() {
   return (
     <>
       <HomeExperience />
       <HomeChromeEnhancer />
+      <NavbarLiveSync />
       <ContinuousMarquees />
       <AnalyticsTracker />
     </>
